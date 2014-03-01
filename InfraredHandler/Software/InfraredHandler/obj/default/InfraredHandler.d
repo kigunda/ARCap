e142accc1c8a46463b2f601a8f3af57603f5bb47
unknown <kigunda@e3-11-19.ece.ualberta.ca>
2014-02-28 17:16:05 -0700

    Starting working on InfraredHandler class to provide high level access to infrared receive and emit actions.

---
+++ InfraredHandler/Software/InfraredHandler/obj/default/InfraredHandler.d
@@ -0,0 +1,53 @@
+obj/default/InfraredHandler.o: InfraredHandler.cpp InfraredHandler.h \
+  ../InfraredHandler_bsp//drivers/inc/altera_up_avalon_de0_nano_adc.h \
+  ../InfraredHandler_bsp//HAL/inc/sys/alt_dev.h \
+  ../InfraredHandler_bsp/system.h ../InfraredHandler_bsp/linker.h \
+  ../InfraredHandler_bsp//HAL/inc/sys/alt_llist.h \
+  ../InfraredHandler_bsp//HAL/inc/alt_types.h \
+  ../InfraredHandler_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../InfraredHandler_bsp//HAL/inc/sys/alt_llist.h \
+  ../InfraredHandler_bsp//HAL/inc/alt_types.h \
+  ../InfraredHandler_bsp//HAL/inc/sys/alt_alarm.h \
+  ../InfraredHandler_bsp//HAL/inc/sys/alt_llist.h \
+  ../InfraredHandler_bsp//HAL/inc/priv/alt_alarm.h \
+  ../InfraredHandler_bsp//HAL/inc/sys/alt_warning.h \
+  ../InfraredHandler_bsp//drivers/inc/altera_up_avalon_de0_nano_adc_regs.h \
+  ../InfraredHandler_bsp//HAL/inc/io.h \
+  ../InfraredHandler_bsp//HAL/inc/alt_types.h \
+  ../InfraredHandler_bsp//drivers/inc/altera_up_avalon_de0_nano_adc_regs.h
+
+InfraredHandler.h:
+
+../InfraredHandler_bsp//drivers/inc/altera_up_avalon_de0_nano_adc.h:
+
+../InfraredHandler_bsp//HAL/inc/sys/alt_dev.h:
+
+../InfraredHandler_bsp/system.h:
+
+../InfraredHandler_bsp/linker.h:
+
+../InfraredHandler_bsp//HAL/inc/sys/alt_llist.h:
+
+../InfraredHandler_bsp//HAL/inc/alt_types.h:
+
+../InfraredHandler_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../InfraredHandler_bsp//HAL/inc/sys/alt_llist.h:
+
+../InfraredHandler_bsp//HAL/inc/alt_types.h:
+
+../InfraredHandler_bsp//HAL/inc/sys/alt_alarm.h:
+
+../InfraredHandler_bsp//HAL/inc/sys/alt_llist.h:
+
+../InfraredHandler_bsp//HAL/inc/priv/alt_alarm.h:
+
+../InfraredHandler_bsp//HAL/inc/sys/alt_warning.h:
+
+../InfraredHandler_bsp//drivers/inc/altera_up_avalon_de0_nano_adc_regs.h:
+
+../InfraredHandler_bsp//HAL/inc/io.h:
+
+../InfraredHandler_bsp//HAL/inc/alt_types.h:
+
+../InfraredHandler_bsp//drivers/inc/altera_up_avalon_de0_nano_adc_regs.h:
